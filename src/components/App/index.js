23181219560b3f54e053b0ee8c53533350a79b85
sonpham <sonpnh95@gmail.com>
2020-07-06 17:37:23 +0700

    update redirect

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 
 import Navigation from '../Navigation';
 import LandingPage from '../Landing';
@@ -13,25 +13,50 @@ import AdminPage from '../Admin';
 import * as ROUTES from '../../constants/routes';
 import { withAuthentication } from '../Session';
 
-const App = () => (
-  <Router>
-    <div>
-      <Navigation />
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      authUser: JSON.parse(localStorage.getItem('authUser'))
+    };
+  }
 
-      <hr />
+  render() {
+    return (
+      <Router>
+        <div>
+          <Navigation />
 
-      <Route exact path={ROUTES.LANDING} component={LandingPage} />
-      <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
-      <Route path={ROUTES.SIGN_IN} component={SignInPage} />
-      <Route
-        path={ROUTES.PASSWORD_FORGET}
-        component={PasswordForgetPage}
-      />
-      <Route path={ROUTES.HOME} component={HomePage} />
-      <Route path={ROUTES.ACCOUNT} component={AccountPage} />
-      <Route path={ROUTES.ADMIN} component={AdminPage} />
-    </div>
-  </Router>
-);
+          <hr />
+          <Switch>
+            <Route
+              exact
+              path="/"
+              render={() => {
+                return (
+                  this.state.authUser === null ?
+                  <Redirect to="/signin" /> :
+                  <Redirect to="/home" />
+                  
+                )
+              }}
+            />
+
+            <Route exact path={ROUTES.LANDING} component={LandingPage} />
+            <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+            <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+            <Route
+              path={ROUTES.PASSWORD_FORGET}
+              component={PasswordForgetPage}
+            />
+            <Route path={ROUTES.HOME} component={HomePage} />
+            <Route path={ROUTES.ACCOUNT} component={AccountPage} />
+            <Route path={ROUTES.ADMIN} component={AdminPage} />
+          </Switch>
+        </div>
+      </Router>
+    )
+  }
+};
 
 export default withAuthentication(App);
